8521625bc4ac361c9bf73783ef0c1f0f78a85c1b
Dmitry Natalchuk <next1996@ukr.net>
2022-07-26 16:25:28 +0300

    add hw 1 module 9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+
+const body = document.querySelector("body")
+const btnStart = document.querySelector("button[data-start]");
+const btnStop = document.querySelector("button[data-stop]");
+
+btnStop.disabled = true;
+let timerId = null;
+
+btnStart.addEventListener("click",onStart)
+btnStop.addEventListener("click",onStop)
+
+function onStart () {
+    btnStart.disabled = true
+    btnStop.disabled = false
+    //console.log("click")
+        timerId = setInterval(() => {
+    body.style.background = getRandomHexColor()
+    },1000)
+};
+
+function onStop(){
+    if(timerId) {
+        clearInterval(timerId);
+    btnStart.disabled = false
+    btnStop.disabled = true
+    }
+    return
+}
